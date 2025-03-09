e42fc25b3cb42efc3c43a3cba42b6094ed3a011c
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-03-09 12:32:52 +0300

    0.0.3

--- script.js
+++ script.js
@@ -4,18 +4,38 @@ const score = document.querySelector('.score');
 const highscore = document.querySelector('.highscore');
 const again = document.querySelector('.again');
 const check = document.querySelector('.check');
+let scoreNumber = 20;
 
-check.addEventListener('click', function() {
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+question.textContent = secretNumber;
+
+check.addEventListener('click', function () {
    const numberInput = Number(document.querySelector('.number-input').value);
    console.log(numberInput, typeof numberInput);
 
    if (!numberInput) {
       guessMessage.textContent = 'Введите число';
+   } else if (numberInput === secretNumber) {
+      guessMessage.textContent = 'Вы угадали';
+   } else if (numberInput > secretNumber) {
+
+      if (scoreNumber > 1) {
+         guessMessage.textContent = 'Загаданное число меньше';
+         scoreNumber--;
+         score.textContent = scoreNumber;
+      } else {
+         guessMessage.textContent = 'Вы проиграли';
+         score.textContent = 0;
+      }
+   } else if (numberInput < secretNumber) {
+      if (scoreNumber > 1 ){
+         guessMessage.textContent = 'Загаданное число больше';
+         scoreNumber--;
+         score.textContent = scoreNumber;
+      } else {
+         guessMessage.textContent = 'Вы проиграли';
+         score.textContent = 0;
+      }
    }
 });
-
-
-
-
-
-
